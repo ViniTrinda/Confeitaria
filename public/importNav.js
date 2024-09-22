10ba9947fef4383e076c6464a152616de30b20de
Vini Trinda <vinitrinda5005@gmail.com>
2024-09-22 16:38:12 -0300

    adicionando nav no site

--- public/importNav.js
+++ public/importNav.js
@@ -1,4 +1,4 @@
-fetch('nav.html').then(response => response.text()).then(data =>{
+fetch('/Confeitaria/nav.html').then(response => response.text()).then(data =>{
     document.getElementsByClassName('header')[0].innerHTML = data;
 }).catch(error => {
     console.error("erro ao carregar o nav: ", error)
